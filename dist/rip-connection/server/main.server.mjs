3a49dcc6a56b5656c73274ed7ccfa8a4d146071b
MJ <51940962+MJ-014@users.noreply.github.com>
2024-08-13 15:10:44 +0330

    Add files via upload

---
+++ dist/rip-connection/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IQHMEK6P.mjs";import"./chunk-YTVNFPJN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+
+export const routes: Routes = [
+    {path: '', component:GameComponent}
+];
